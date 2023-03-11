80a6ed2c694595861ad1977900473ee07d6f69c0
Draster <jorlan.jesus@gmail.com>
2023-03-11 11:31:09 -0600

    merge to linux env day 10

---
+++ Day10/Functions.py
@@ -0,0 +1,14 @@
+
+
+def format_name (name, lastName): 
+    """Return the formatted name of the user"""
+
+    return f"{name.tittle()} {lastName.tittle()}"
+
+
+
+
+
+
+
+print(format_name("jesus","zapata"))
